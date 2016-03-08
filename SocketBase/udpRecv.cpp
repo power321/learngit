02c9970768cf47d97cca629a345b5f0c18c7957f
chenxian <chenxian_0@163.com>
2016-03-08 12:10:07 +0800

    Udp S/R Time Test && Set S/R Buff Size

--- SocketBase/udpRecv.cpp
+++ SocketBase/udpRecv.cpp
@@ -2,21 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 const int PORT = 9876;
 const char IP_DEST[10] = "127.0.0.1";
 
+//Time Subtract
+/*
+ * int timeSub(struct timeval *result, struct timeval *start, struct timeval *stop)
+{
+	int nsec;
+	if(x->tv_sec > y->tv_sec)
+		return -1;
+	if((x->tv_sec == y->tv_sec) && (x->tv_usec > y->tv_usec))
+		return -1;
+	result->tv_sec = (y->tv_sec - x->tv_sec);
+	result->tv_usec = (y->tv_usec - x->tv_usec);
+	if(result->tv_usec < 0)
+	{
+		result->tv_sec--;
+		result->tv_usec += 1000000;
+	}
+	return 0;
+}
+*/
 int main()
 {
-	char buf[1024];
+	//Time Test
+	struct timeval start, stop, diff;
+
+	//Socket Buffer Maxinum
+	int rcv_size = 0;
+	int snd_size = 0;
+	int err = -1;
+	socklen_t optlen;
+
+	char buf[8*1024];
 	struct sockaddr_in addrSrc;
 	memset((void*)&addrSrc, 0, sizeof(addrSrc));
 	addrSrc.sin_family = AF_INET;
 	addrSrc.sin_port = htons(PORT);
-	addrSrc.sin_addr.s_addr = inet_addr(IP_DEST);
+	addrSrc.sin_addr.s_addr = htonl(INADDR_ANY);
+	//addrSrc.sin_addr.s_addr = inet_addr(IP_DEST);
 	socklen_t addrSrc_len = sizeof(addrSrc);
 
 	int udpSocket = socket(AF_INET, SOCK_DGRAM, 0);//IPv4 UDP
@@ -25,6 +56,58 @@ int main()
 		printf("Create UDP Socket Error!\n");
 		return -1;
 	}
+
+	optlen = sizeof(rcv_size);
+	err = getsockopt(udpSocket, SOL_SOCKET, SO_RCVBUF, &rcv_size, &optlen);
+	if(err < 0)
+	{
+		printf("Get Recv Buff Size Error!\n");
+		return -1;
+	}
+	printf("System Socket Recv Buff Size: %d Bytes\n", rcv_size);
+	
+	optlen = sizeof(snd_size);
+	err = getsockopt(udpSocket, SOL_SOCKET, SO_SNDBUF, &snd_size, &optlen);
+	if(err < 0)
+	{
+		printf("Get Send Buff Size Error!\n");
+		return -1;
+	}
+	printf("System Socket Send Buff Size: %d Bytes\n", snd_size);
+	
+	rcv_size = 80 * 1024;
+	optlen = sizeof(rcv_size);
+	err = setsockopt(udpSocket, SOL_SOCKET, SO_RCVBUF, &rcv_size, optlen);
+	if(err < 0)
+	{
+		printf("Set Recv Buff Size Error!\n");
+		return -1;
+	}
+	optlen = sizeof(rcv_size);
+	err = getsockopt(udpSocket, SOL_SOCKET, SO_RCVBUF, &rcv_size, &optlen);
+	if(err < 0)
+	{
+		printf("Get Recv Buff Size(My) Error!\n");
+		return -1;
+	}
+	printf("System Socket Recv Buff Size(My): %d Bytes\n", rcv_size);
+	
+	snd_size = 80 * 1024;
+	optlen = sizeof(snd_size);
+	err = setsockopt(udpSocket, SOL_SOCKET, SO_SNDBUF, &snd_size, optlen);
+	if(err < 0)
+	{
+		printf("Set Send Buff Size Error!\n");
+		return -1;
+	}
+	optlen = sizeof(snd_size);
+	err = getsockopt(udpSocket, SOL_SOCKET, SO_SNDBUF, &snd_size, &optlen);
+	if(err < 0)
+	{
+		printf("Get Send Buff Size(My) Error!\n");
+		return -1;
+	}
+	printf("System Socket Send Buff Size(My): %d Bytes\n", snd_size);
 
 	int result = bind(udpSocket, (struct sockaddr*)&addrSrc, sizeof(addrSrc));
 	if(result < 0)
@@ -38,14 +121,23 @@ int main()
 	while(1)
 	{
 		recvNum = recvfrom(udpSocket, buf, sizeof(buf), 0, (struct sockaddr*)&addrSrc, &addrSrc_len);
+		if(recvTotal == 0)
+			gettimeofday(&start, 0);
 		recvTotal += recvNum;
-		printf("Received from client:%s\n", buf);
+		//printf("Received from client:%s\n", buf);
 		if(strncmp(buf, "stop", 4) == 0) // End Receive
 		{
 			printf("Sender told me to end the connection!\n");
 			break;
 		}
 	}
+	gettimeofday(&stop, 0);
+	printf("Get %d bytes from udp client\n", recvTotal);
+	//timeSub(&diff, &start, &stop);
+	//printf("Use time: %dms\n", diff.tv_usec);
+	double timeUsed = 1000000 * (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec); //microsecond
+	timeUsed /= 1000; //millisecond
+	printf("Use time: %.2lf ms\n", timeUsed);
 
 	close(udpSocket);
 	return 0;
--- SocketBase/udpSend.cpp
+++ SocketBase/udpSend.cpp
@@ -5,13 +5,16 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 const int PORT = 9876;
 const char IP_DEST[10] = "127.0.0.1";
 
 int main()
 {
+	struct timeval start, stop;
 	char buf[1024];
+	memset(buf, 0, sizeof(buf));
 	struct sockaddr_in addrDest;
 	memset((void*)&addrDest, 0, sizeof(addrDest));
 	addrDest.sin_family = AF_INET;
@@ -24,14 +27,26 @@ int main()
 		printf("Create UDP Socket Error!\n");
 		return -1;
 	}
-	for(int iter=0; iter<=20; ++iter)
+	int sendTotal = 0;
+	int sendNum = 0;
+	gettimeofday(&start, 0);
+	for(int iter=0; iter<8*1024; ++iter)
 	{
-		sprintf(buf, "data packet with ID %d\0", iter);
-		sendto(udpSocket, buf, sizeof(buf), 0, (struct sockaddr *)&addrDest, sizeof(addrDest));
+		//sprintf(buf, "data packet with ID %d\0", iter);
+		sendNum = sendto(udpSocket, buf, sizeof(buf), 0, (struct sockaddr *)&addrDest, sizeof(addrDest));
+		sendTotal += sendNum;
 	}
 	sprintf(buf, "stop\0");
-	sendto(udpSocket, buf, sizeof(buf), 0, (struct sockaddr *)&addrDest, sizeof(addrDest));
+	sendNum = sendto(udpSocket, buf, sizeof(buf), 0, (struct sockaddr *)&addrDest, sizeof(addrDest));
+	sendTotal += sendNum;
+	gettimeofday(&stop, 0);
+
+	double timeUsed = 1000000 * (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec);
+	timeUsed /= 1000;
 	
+	printf("Send udp data %d bytes\n", sendTotal);
+	printf("Use time %.2lf ms\n", timeUsed);
+
 	close(udpSocket);
 	printf("Terminating!\n");
 
